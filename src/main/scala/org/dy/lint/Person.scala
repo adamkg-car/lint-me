496e0174b10a260cabddb46e65d9bf4c22c556cb
at15 <at15@outlook.com>
2015-05-29 21:16:59 +0800

    scala goat is working
    but why linter and my own linter are not working?

--- src/main/scala/org/dy/lint/Person.scala
+++ src/main/scala/org/dy/lint/Person.scala
@@ -4,9 +4,14 @@ import org.dy.lint.Cat
 
 object Hi {
   def main(args: Array[String]) = {
-    val a = 123
+    var a = 123
     val d = 3 / 0
     val c = new Cat
+    if(true){
+      a = 2
+    }else{
+      a= 3
+    }
     println("Hi Lint me2!")
   }
 }
